6b548f07821b14e48a5058ad7b97239492ef3714
Eric <eric.hennenfent@trailofbits.com>
2023-06-11 15:50:04 -0700

    Big refactor of turtle wrapper

--- python/drawbot_control/turtle_wrapper.py
+++ python/drawbot_control/turtle_wrapper.py
@@ -1,23 +1,59 @@
-from turtle import Screen, RawTurtle, Vec2D
+from turtle import Screen, RawTurtle, Vec2D, _Screen
 from dataclasses import dataclass
-import time
+from time import sleep
+from functools import partial
+import typing as t
 
 X_WIDTH = 400
 Y_HEIGHT = 400
 
+AnonFunc: t.TypeAlias = t.Callable[[], None]
 
-@dataclass
+
+@dataclass(frozen=True)
 class MoveRecord:
     dest: Vec2D
     pen_down: bool
 
 
-@dataclass
+@dataclass(frozen=True)
 class SleepRecord:
     duration: float
 
 
-def _init_screen():
+@dataclass(frozen=True)
+class TurtleState:
+    position: Vec2D
+    heading: float
+    pen_down: bool
+
+
+@dataclass(frozen=True)
+class _PartialTurtle:
+    forward: AnonFunc
+    backward: AnonFunc
+    left: AnonFunc
+    right: AnonFunc
+    penup: AnonFunc
+    pendown: AnonFunc
+    push_state: AnonFunc
+    pop_state: AnonFunc
+
+    @classmethod
+    def from_turtle(cls, turtle: "Drawbot", length: float, angle: float) -> "_PartialTurtle":
+        return cls(
+            forward=partial(turtle.forward, length),
+            backward=partial(turtle.backward, length),
+            left=partial(turtle.left, angle),
+            right=partial(turtle.right, angle),
+            penup=turtle.penup,
+            pendown=turtle.pendown,
+            push_state=turtle.push_state,
+            pop_state=turtle.pop_state,
+        )
+
+
+def _init_screen() -> _Screen:
     screen = Screen()
     screen.setworldcoordinates(0, 0, X_WIDTH, Y_HEIGHT)
     screen.screensize(X_WIDTH * 2, Y_HEIGHT * 2)
@@ -26,90 +62,175 @@ def _init_screen():
     return screen
 
 
-class Turtle:
-    def __init__(self):
-        self.screen = _init_screen()
-        self.turtle = RawTurtle(canvas=self.screen, undobuffersize=0)
-        self.turtle.speed(0)
+class _TempPenState:
+    def __init__(self, turtle: "Drawbot", pen_down: bool):
+        self.turtle = turtle
+        self.pen_down = pen_down
+        self.old_pen_down = self.turtle.isdown()
 
-        self._pendown = self.turtle.isdown()
-        self._movements = [MoveRecord((0, 0), False)]
+    def __enter__(self) -> None:
+        self.old_pen_down = self.turtle.isdown()
+        self.turtle.set_pen_down(self.pen_down)
+
+    def __exit__(self, *_args: t.Any) -> None:
+        self.turtle.set_pen_down(self.old_pen_down)
+
+
+class Drawbot:
+    def partial(self, length: float, angle: float) -> _PartialTurtle:
+        return _PartialTurtle.from_turtle(self, length, angle)
+
+    def __init__(self, *, hide_inactive_moves: bool = False, speed: int = 0) -> None:
+        self._screen: _Screen = _init_screen()
+        self._turtle: RawTurtle = RawTurtle(canvas=self._screen, undobuffersize=0)
+        self._turtle.speed(speed)
+
+        self.hide_inactive_moves: bool = hide_inactive_moves
+
+        self._pendown: bool = self._turtle.isdown()
+        self._movements: t.List[t.Union[MoveRecord, SleepRecord]] = [MoveRecord(self.position(), False)]
+        self._state_stack: t.List[TurtleState] = []
 
     # Motion
-    def forward(self, distance):
-        self.turtle.forward(distance)
+    def forward(self, distance: float) -> None:
+        self._turtle.forward(distance)
         self._movements.append(MoveRecord(self.position(), self.isdown()))
 
-    def backward(self, distance):
-        self.turtle.backward(distance)
+    fd = forward
+
+    def backward(self, distance: float) -> None:
+        self._turtle.backward(distance)
         self._movements.append(MoveRecord(self.position(), self.isdown()))
 
-    def goto(self, *args):
-        self.turtle.goto(*args)
+    bk = backward
+    back = backward
+
+    def goto(self, x: t.Union[float, Vec2D], y: t.Optional[float] = None) -> None:
+        self._turtle.goto(x, y)
         self._movements.append(MoveRecord(self.position(), self.isdown()))
 
-    def setx(self, x):
+    setpos = goto
+    setposition = goto
+
+    def setx(self, x: float) -> None:
         return self.goto(x, self.ycor())
 
-    def sety(self, y):
+    def sety(self, y: float) -> None:
         return self.goto(self.xcor(), y)
 
-    def home(self, *args):
+    def home(self) -> None:
         self.goto(0, 0)
         self.setheading(0)
 
     # Angle manipulation
-    def right(self, angle):
-        self.turtle.right(angle)
+    def right(self, angle: float) -> None:
+        self._turtle.right(angle)
 
-    def left(self, angle):
-        self.turtle.left(angle)
+    rt = right
 
-    def degrees(self, *args):
-        self.turtle.degrees(*args)
+    def left(self, angle: float) -> None:
+        self._turtle.left(angle)
 
-    def radians(self, *args):
-        self.turtle.radians(*args)
+    lt = left
 
-    def setheading(self, *args):
-        self.turtle.setheading(*args)
+    def degrees(self, fullcircle: float = 360.0) -> None:
+        self._turtle.degrees(fullcircle)
+
+    def radians(self) -> None:
+        self._turtle.radians()
+
+    def setheading(self, to_angle: float) -> None:
+        self._turtle.setheading(to_angle)
+
+    seth = setheading
 
     # Pen Controls
-    def penup(self, *args):
+    def penup(self) -> None:
         self._pendown = False
-        self.turtle.pencolor("#FFFFBB")
+        self._turtle.pencolor("#FFFFBB")
+        if self.hide_inactive_moves:
+            self._turtle.penup()
 
-    def pendown(self, *args):
+    pu = penup
+    up = penup
+
+    def pendown(self) -> None:
         self._penup = True
-        self.turtle.pencolor("#000000")
+        self._turtle.pencolor("#000000")
+        if self.hide_inactive_moves:
+            self._turtle.pendown()
 
-    def isdown(self, *args):
+    pd = pendown
+    down = pendown
+
+    def isdown(self) -> bool:
         return self._pendown
 
+    def set_pen_down(self, new_pen_down: bool) -> None:
+        if new_pen_down:
+            self.pendown()
+        else:
+            self.penup()
+
     # Read turtle state
-    def position(self):
-        return self.turtle.position()
+    def position(self) -> Vec2D:
+        return self._turtle.position()
 
-    def towards(self, *args):
-        return self.turtle.towards(*args)
+    pos = position
 
-    def xcor(self):
-        return self.turtle.xcor()
+    def towards(self, x: t.Union[float, Vec2D], y: t.Optional[float] = None) -> float:
+        return self._turtle.towards(x, y)
 
-    def ycor(self):
-        return self.turtle.ycor()
+    def xcor(self) -> float:
+        return self._turtle.xcor()
 
-    def heading(self):
-        return self.turtle.heading()
+    def ycor(self) -> float:
+        return self._turtle.ycor()
 
-    def distance(self, *args):
-        return self.turtle.distance(*args)
+    def heading(self) -> float:
+        return self._turtle.heading()
+
+    def distance(self, x: t.Union[float, Vec2D], y: t.Optional[float] = None) -> float:
+        return self._turtle.distance(x, y)
 
     # Screen controls
-    def bgpic(self, *args):
-        self.screen.bgpic(*args)
+    def bgpic(self, new_pic: t.Optional[str] = None) -> t.Optional[str]:
+        return self._screen.bgpic(new_pic)
 
     # Supplemental
-    def wait(self, time):
+    def wait(self, time: float) -> None:
         self._movements.append(SleepRecord(time))
-        time.sleep(time)
+        sleep(time)
+
+    def push_state(self) -> None:
+        self._state_stack.append(
+            TurtleState(
+                position=self.position(),
+                heading=self.heading(),
+                pen_down=self.isdown(),
+            )
+        )
+
+    def pop_state(self, position: bool = True, heading: bool = True, pen_down: bool = False) -> None:
+        last_state: TurtleState = self._state_stack.pop()
+        if position:
+            self.goto(last_state.position)
+        if heading:
+            self.setheading(last_state.heading)
+        if pen_down:
+            self.set_pen_down(last_state.pen_down)
+
+    # Wrappers for forcing pen state during certain operations
+    def TempPenState(self, pen_down: bool) -> _TempPenState:
+        return _TempPenState(self, pen_down)
+
+    def _wrap_with_pen_state(self, pen_down: bool, func: t.Callable[[t.Any], t.Any], *args: t.Any) -> t.Any:
+        with self.TempPenState(pen_down):
+            return func(*args)
+
+    def jog(self, distance: float) -> None:
+        self._wrap_with_pen_state(False, self.forward, distance)
+
+    def teleport(self, x: t.Union[float, Vec2D], y: t.Optional[float] = None) -> None:
+        self._wrap_with_pen_state(False, self.goto, x, y)
+
