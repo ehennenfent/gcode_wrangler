aa9e53a85b206c2ac1d4d179ed1acdc7b636edbb
Eric <eric.hennenfent@trailofbits.com>
2023-06-11 21:51:57 -0700

    Super basic sketch for client

--- client/drawbot_client/client.py
+++ client/drawbot_client/client.py
@@ -1,3 +1,27 @@
+from drawbot_client import Drawbot
+
+class SessionManager():
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        pass
+
+    def drawbot(self):
+        return Drawbot()
+    
+    def queue(self):
+        pass
 
 class Client:
-    pass
+    
+    def __init__(self, host, sessionkey, username):
+        self.host = host
+        self.sessionkey = sessionkey
+        self.username = username
+
+    def session():
+        return SessionManager()
+    
+
