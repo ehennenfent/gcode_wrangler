7591c323fd32128f50437dae059d5692ef60b5a8
Eric <eric.hennenfent@trailofbits.com>
2023-06-17 02:43:20 -0700

    Basic background tasks working

--- server/drawbot_server/main.py
+++ server/drawbot_server/main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from time import sleep
 
 from pydantic import BaseModel
 
 class GcodeJob(BaseModel):
     image_path: str = "https://fakeimg.pl/800x600"
     comment: str = ""
+    progress: int = 0
+
+    @property
+    def get_progress(self):
+        return f"{self.progress}%"
+    
+    def start_background_task(self):
+        self.progress = 0
+        for _ in range(100):
+            self.progress += 1
+            sleep(1)
 
 app = FastAPI()
 
@@ -33,3 +45,13 @@ async def get_progress(request: Request):
 @app.get("/job_list", response_class=HTMLResponse)
 async def get_progress(request: Request):
     return templates.TemplateResponse("job_list.html", {"request": request, "jobs": all_jobs})
+
+
+@app.get("/progress", response_class=HTMLResponse)
+async def get_progress(request: Request):
+    return templates.TemplateResponse("progress.html", {"request": request, "primary_job": primary_job})
+
+@app.post("/run_current")
+async def start_background_task(background_tasks: BackgroundTasks):
+    background_tasks.add_task(primary_job.start_background_task)
+    return "Go"
