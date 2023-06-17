d7dfe5f263ceeb833e46c24eaed1c4d2daed612e
Eric <eric.hennenfent@trailofbits.com>
2023-06-16 19:47:52 -0700

    Better placeholders, actual fonts

--- server/drawbot_server/main.py
+++ server/drawbot_server/main.py
@@ -3,6 +3,12 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from pydantic import BaseModel
+
+class GcodeJob(BaseModel):
+    image_path: str = "https://fakeimg.pl/800x600"
+    comment: str = ""
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -11,6 +17,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+primary_job = GcodeJob()
+all_jobs = [GcodeJob(comment="foobar"),
+            GcodeJob(comment="barfoo"),
+            GcodeJob(comment="A"*64)]
+
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "items": ["first", "second", "Thisoneisreallylong AAAAAAAAAAAAAAAAAAAAAAA"]})
+    return templates.TemplateResponse("index.html", {"request": request,
+                                                     "primary_job": primary_job,
+                                                     "jobs": all_jobs})
